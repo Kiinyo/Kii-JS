df02b69fa1a7f2d6e28ee894625dd4d9cdd11a9f
Kiinyo <iiyoheki@gmail.com>
2021-01-03 21:08:22 -0700

    Removed Game.Faunas

--- src/content/compendium.js
+++ src/content/compendium.js
@@ -47,19 +47,3 @@ Game.Compendium = function (template) {
         }
     }
 }
-
-
-Game.Faunas = {
-    Animal: {
-      name: 'Wild Animal',
-      desc: 'Try not to bother it!',
-      type: 'Fauna',
-  
-      x: 2,
-      y: 0,
-  
-      fgColor: 'yellow',
-      glyph: 'W',
-      traits: [Kii.Traits.Tangible]
-    }
-}
