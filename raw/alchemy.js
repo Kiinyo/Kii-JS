ed1fff4dfd6f036581e84aaeeba6d3e10c47f87c
Kiinyo <iiyoheki@gmail.com>
2021-01-05 17:49:32 -0700

    removed compendium in preparation for world build

---
+++ raw/alchemy.js
@@ -0,0 +1,54 @@
+Kii.Alchemy = {
+    Domains: {
+        Apothecary: { //Affects living things
+            Life_Force: { //Affects HP, can restore or drain (poison)
+                //What it affects
+                target: '_lifeForce',
+                //How much it affects per unit of size
+                magnitude: 200,
+                //How many different types exist
+                variation: 3,
+                //Does it have an 'antidote' or reverse effect
+                reversable: true
+            },
+            Energy: { //Affects Energy/MP, can restore or drain (sleep)
+                target: '_energy',
+                magnitude: 10000,
+                variation: 3,
+                reversable: true
+            },
+            Stamina: { //Affects Stamina, can restore or drain (paralyze)
+                target: '_stamina',
+                magnitude: 1000,
+                variation: 3,
+                reversable: true
+            },
+            Disease: { //Can cure 3 different variations of Disease
+                target: 'Diseases',
+                magnitude: -100,
+                variation: 3,
+                reverseable: false
+            }
+        },
+        Alteration: { //Affects physical properties
+            Bouyancy: {
+                target: 'Bouyancy',
+                magnitude: 100,
+                variation: 1,
+                reversable: true
+            },
+            Refraction: {
+                target: 'Refraction',
+                magnitude: 100,
+                variation: 1,
+                reversable: true
+            },
+            Size: {
+                target: 'Size',
+                magnitude: 100,
+                variation: 1,
+                reversable: true
+            }
+        }
+    }
+}
